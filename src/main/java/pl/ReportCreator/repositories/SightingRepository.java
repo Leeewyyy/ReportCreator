5e4e85f113bc9d77bffe44cbe990cd5e4b5322d4
Leeewyyy <adam.lewy6@wp.pl>
2022-07-05 07:31:40 -0700

    Added repository notation

--- src/main/java/pl/ReportCreator/repositories/SightingRepository.java
+++ src/main/java/pl/ReportCreator/repositories/SightingRepository.java
@@ -1,7 +1,9 @@
 package pl.ReportCreator.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import pl.ReportCreator.entities.Sighting;
 
+@Repository
 public interface SightingRepository extends CrudRepository<Sighting, Long> {
 }
