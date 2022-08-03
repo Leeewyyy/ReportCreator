08a9e1ad13c3f203b67786b7c0a23dadeafd0401
Leeewyyy <adam.lewy6@wp.pl>
2022-08-03 18:33:03 +0200

    Commented hard-coded database updates

--- src/main/java/pl/ReportCreator/Start.java
+++ src/main/java/pl/ReportCreator/Start.java
@@ -17,11 +17,11 @@ public class Start {
         this.repo = repo;
     }
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void runExample() {
-        Sighting s = new Sighting(18021992647L, "3D Mark - VRS Tier 2 - Tearing + Stuttering", "11155 + 210B", "p1-showstopper", false, "Yes", "complete", "d3d12");
-        Sighting s2 = new Sighting(18017757210L, "3DMark - little pixels corruptions in VRS Tier 1 test", "8968 + 1320", "p1-showstopper", false, "No", "development", "d3d12");
-        repo.save(s);
-        repo.save(s2);
-    }
+//    @EventListener(ApplicationReadyEvent.class)
+//    public void runExample() {
+//        Sighting s = new Sighting(18021992647L, "3D Mark - VRS Tier 2 - Tearing + Stuttering", "11155 + 210B", "p1-showstopper", false, "Yes", "complete", "d3d12");
+//        Sighting s2 = new Sighting(18017757210L, "3DMark - little pixels corruptions in VRS Tier 1 test", "8968 + 1320", "p1-showstopper", false, "No", "development", "d3d12");
+//        repo.save(s);
+//        repo.save(s2);
+//    }
 }
